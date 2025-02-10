4357df7a1bf750c1cc7bb0be9d5af1537440b40c
leen <leen051206@gmail.com>
2025-02-10 16:03:28 +0200

    feat: add ProductList component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
 
+//q1
+function UserList(props){
+  console.log(props.userList);
+  return <ul>
+    {props.userList.map((item,index) => {
+      return <li>key={index} - {item.name} {item.email}</li>
+})}
+  </ul>
+}
+
+//q2
+function ProductList(props){
+  console.log(props.productsList);
+  return <ul>
+    {props.userList.map((item,index) => {
+      return <li>key={index} - {item.name} {item.price}</li>
+})}
+  </ul>
+}
+
+
+
 function App() {
+  const myList = [
+    {id:1, name:'Leen', email:'leen05@gmail.com'},
+    {id:2, name:'Lady', email:'lady12@gmail.com'}
+  ];
+  const products = [
+    {id:1, name:'Laptob', price:5000},
+    {id:2, name:'Ipad', price:2000}
+  ];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <UserList userList={myList}></UserList>
+      <ProductList productsList={products}></ProductList>
     </div>
   );
 }
