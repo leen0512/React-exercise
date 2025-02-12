b518613b83dc36c6971ae8f4d831138133f8ad69
leen <leen051206@gmail.com>
2025-02-12 10:00:18 +0200

    increasing number function using state

---
+++ react-state/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+
+//q1
+function ShowNumber(){
+  const [count, setCount] = useState(1);
+  function IncreaseNumber(){
+    setCount(count+1);
+  }
+  return <div>
+    <h1>{count}</h1>
+    <button onClick={IncreaseNumber}>Click Here to Increase Number</button>
+  </div>
+}
+
+function App() {
+  return (
+    <div className="App">
+      <ShowNumber title="Increase Number"></ShowNumber>
+    </div>
+  );
+}
+
+export default App;
